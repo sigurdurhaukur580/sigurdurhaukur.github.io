b620029ccb2dde2e68be4c48247a26b96a2c23e4
sigurdurhaukur <sigurdur.h.b@icloud.com>
2020-10-20 15:39:28 +0000

    added more content

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ window.addEventListener('scroll', function(e) {
             target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
         }
     }
-
-
 });
+
+
+let section = document.querySelector('.information');
+function inView (el) {
+    var sb = section.getBoundingClientRect();
+    var eb = el.getBoundingClientRect();
+    return !((eb.top + eb.height < 0) || (eb.top > sb.height));
+  }
+  
+  function updateInView() {
+    for (x of document.querySelectorAll('.info')) {
+      if (inView(x)) x.classList.add('inview')
+      else x.classList.remove('inview');
+    }
+  }
+  
+  section.onscroll = updateInView;
+  
+  updateInView();
