b445d6a972285cd4da8f0bb33fdcfc5a996a06fc
Sigurdur Haukur Birgisson <sigurdur.h.b@icloud.com>
2020-11-03 09:24:58 +0000

    aldjj

--- script.js
+++ script.js
@@ -17,20 +17,3 @@ window.addEventListener('scroll', function(e) {
         }
     }
 });
-
-
-const articles = document.querySelectorAll('.item');
-
-observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
-
-articles.forEach(article => {
-  observer.observe(article);
-});
